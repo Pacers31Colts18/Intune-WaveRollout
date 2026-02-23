d4e399b459fa38c4b30709595e4430b6df0e6237
Joe Loveless <jloveless03@pm.me>
2026-02-23 14:47:12 -0600

    Update Import-IntuneSettingsCatalogPolicy.ps1

--- Public/Import-IntuneSettingsCatalogPolicy.ps1
+++ Public/Import-IntuneSettingsCatalogPolicy.ps1
@@ -1,29 +1,29 @@
 function Import-IntuneSettingsCatalogPolicy {
-[CmdletBinding()]
-param
-(
-    [parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()]
-    [string]$folder
-)
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$folder
+    )
 
 
-$policyfiles = Get-ChildItem $folder | Select-Object Name, BaseName
+    $policyfiles = Get-ChildItem $folder | Select-Object Name, BaseName
 
-Foreach ($policyfile in $policyfiles){
-    $policyName = $policyfile.Name
-    $policybaseName = $policyfile.BaseName
+    Foreach ($policyfile in $policyfiles) {
+        $policyName = $policyfile.Name
+        $policybaseName = $policyfile.BaseName
 
         $policy = Get-Content -path $folder\$policyName
         $policyCheck = (Invoke-Mggraphrequest -uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies?`$filter=Name eq '$policyBaseName'" -Method GET).value
         
-        if ($policyCheck.Name){
-        Write-Host "$($policyCheck.Name) already exists, modifying profile with PUT"
-        $put = Invoke-Mggraphrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/$($policyCheck.Id)" -Method PUT -Body $policy -ContentType "application/json"
+        if ($policyCheck.Name) {
+            Write-Host "$($policyCheck.Name) already exists, modifying profile with PUT"
+            $put = Invoke-Mggraphrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/$($policyCheck.Id)" -Method PUT -Body $policy -ContentType "application/json"
         }
-        else{
+        else {
             Write-Host "$policybaseName does not exist, creating new profile"
             $post = Invoke-Mggraphrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method POST -Body $policy -ContentType "application/json"
+        }
     }
-}
 }
